169de080c9f070190be233d4e65a9f19957d428d
Dar Dualba <ddualba@gmail.com>
2020-10-14 11:25:54 -0700

    initial commit up to create and animate mobile navigation

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {
+	BrowserRouter as Router,
+	Route,
+	Redirect,
+	Switch
+} from 'react-router-dom';
+
+import Users from './user/pages/Users';
+import NewPlace from './places/pages/NewPlace';
+import MainNavigation from './shared/components/Navigation/MainNavigation';
+
+const App = () => {
+	return (
+		<Router>
+			<MainNavigation />
+			<main>
+				<Switch>
+					<Route path="/" exact component={Users} />
+					<Route path="/places/new" exact component={NewPlace} />
+					<Redirect to="/" />
+				</Switch>
+			</main>
+		</Router>
+	);
+};
+
+export default App;
