b8a37d593f9ee41557b45070e8c9231755431966
Dar Dualba <ddualba@gmail.com>
2020-10-14 21:15:47 -0700

    Filter user places, add styled button component

--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 import PlaceList from "../components/PlaceList";
 
@@ -6,7 +7,8 @@ const DUMMY_PLACES = [
   {
     id: "p1",
     title: "Empire State Building",
-    description: "One of the most famous sky scrapers in the world!",
+    description:
+      "In opinion of User 1, One of the most famous sky scrapers in the world!",
     imageUrl:
       "https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg",
     address: "20 W 34th St, New York, NY 10001",
@@ -19,7 +21,8 @@ const DUMMY_PLACES = [
   {
     id: "p2",
     title: "Empire State Building",
-    description: "One of the most famous sky scrapers in the world!",
+    description:
+      "In opinion of User 2, One of the most famous sky scrapers in the world!",
     imageUrl:
       "https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg",
     address: "20 W 34th St, New York, NY 10001",
@@ -32,7 +35,9 @@ const DUMMY_PLACES = [
 ];
 
 const UserPlaces = () => {
-  return <PlaceList items={DUMMY_PLACES} />;
+  const userId = useParams().userId;
+  const loadedPlaces = DUMMY_PLACES.filter((place) => place.creator === userId);
+  return <PlaceList items={loadedPlaces} />;
 };
 
 export default UserPlaces;
