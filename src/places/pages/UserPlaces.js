41bd71b5cb64968080ae1e1b5ee17fba0bc70af7
Dar Dualba <ddualba@gmail.com>
2020-10-21 20:39:08 -0700

    Show deletion warning

--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -1,50 +1,50 @@
-import React from "react";
-import { useParams } from "react-router-dom";
+import React from 'react';
+import { useParams } from 'react-router-dom';
 
-import PlaceList from "../components/PlaceList";
+import PlaceList from '../components/PlaceList';
 
 const DUMMY_PLACES = [
   {
-    id: "p1",
-    title: "Empire State Building",
+    id: 'p1',
+    title: 'Empire State Building',
     description:
-      "In opinion of User 1, One of the most famous sky scrapers in the world!",
+      'In opinion of User 1, One of the most famous sky scrapers in the world!',
     imageUrl:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg",
-    address: "20 W 34th St, New York, NY 10001",
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg',
+    address: '20 W 34th St, New York, NY 10001',
     location: {
       lat: 40.7484405,
-      lng: -73.9878584,
+      lng: -73.9878584
     },
-    creator: "u1",
+    creator: 'u1'
   },
   {
-    id: "p2",
-    title: "Empire State Building",
+    id: 'p2',
+    title: 'Empire State Building',
     description:
-      "In opinion of User 2, One of the most famous sky scrapers in the world!",
+      'In opinion of User 2, One of the most famous sky scrapers in the world!',
     imageUrl:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg",
-    address: "20 W 34th St, New York, NY 10001",
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg',
+    address: '20 W 34th St, New York, NY 10001',
     location: {
       lat: 40.7484405,
-      lng: -73.9878584,
+      lng: -73.9878584
     },
-    creator: "u2",
+    creator: 'u2'
   },
   {
-    id: "p3",
-    title: "Quail Hill",
-    description: "A nice neighborhood in Irvine",
+    id: 'p3',
+    title: 'Quail Hill',
+    description: 'A nice neighborhood in Irvine',
     imageUrl:
-      "https://www.irvinestandard.com/images/2019/04/center-hero-quailhillshoppingcenter-201808_1600x960.jpg",
-    address: "6875 Quail Hill Pkwy #6601, Irvine, CA 92603",
+      'https://www.irvinestandard.com/images/2019/04/center-hero-quailhillshoppingcenter-201808_1600x960.jpg',
+    address: '6875 Quail Hill Pkwy #6601, Irvine, CA 92603',
     location: {
       lat: 33.6569302,
-      lng: -117.774757,
+      lng: -117.774757
     },
-    creator: "u2",
-  },
+    creator: 'u2'
+  }
 ];
 
 const UserPlaces = () => {
