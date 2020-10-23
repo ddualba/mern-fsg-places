5355ad28013cf6be0dfca9b0f78c826ec48b0403
ddualba <ddualba@gmail.com>
2020-10-23 16:22:08 -0700

    Add auth context, add auth and unAuth routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useCallback} from 'react';
 import {
   BrowserRouter as Router,
   Route,
@@ -12,22 +12,52 @@ import NewPlace from './places/pages/NewPlace';
 import UpdatePlace from './places/pages/UpdatePlace';
 import UserPlaces from './places/pages/UserPlaces';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
+import { AuthContext } from './shared/context/auth-context'
 
 const App = () => {
+  const [ isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const login = useCallback(() => {
+    setIsLoggedIn(true)
+  }, [])
+
+  const logout = useCallback(() => {
+    setIsLoggedIn(false)
+  }, [])
+
+  let routes;
+
+  if (isLoggedIn){
+    routes = (
+      <Switch>
+      <Route path="/" exact component={Users} />
+      <Route path="/:userId/places" exact component={UserPlaces} />
+      <Route path="/places/new" exact component={NewPlace} />
+      <Route path="/places/:placeId" exact component={UpdatePlace} />
+      <Redirect to="/" />
+    </Switch>
+    );
+  } else {
+    routes = (
+      <Switch>
+        <Route path="/" exact component={Users} />
+        <Route path="/:userId/places" exact component={UserPlaces} />
+        <Route path="/auth" exact component={Auth} />
+        <Redirect to="/auth" />
+      </Switch>
+    );
+  }
+  
+
   return (
+    <AuthContext.Provider value={ {isLoggedIn: isLoggedIn, login: login, logout: logout}}>
     <Router>
       <MainNavigation />
       <main>
-        <Switch>
-          <Route path="/" exact component={Users} />
-          <Route path="/auth" exact component={Auth} />
-          <Route path="/:userId/places" exact component={UserPlaces} />
-          <Route path="/places/new" exact component={NewPlace} />
-          <Route path="/places/:placeId" exact component={UpdatePlace} />
-          <Redirect to="/" />
-        </Switch>
+          {routes}
       </main>
     </Router>
+    </AuthContext.Provider>
   );
 };
 
